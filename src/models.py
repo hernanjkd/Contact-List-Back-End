c682ced6c894fd17b40f25c3b6cb26be679240b4
Hernán García <hernanjkd@gmail.com>
2019-06-12 15:35:53 +0000

    serialize with id

--- src/models.py
+++ src/models.py
@@ -14,6 +14,7 @@ class Person(db.Model):
 
     def serialize(self):
         return {
+            "id": self.id,
             "username": self.fullname,
             "email": self.email,
             "phone": self.phone,
